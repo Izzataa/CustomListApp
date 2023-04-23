34edd82489be3e7c6914e34b8c1c8c50fd8f6a0d
izzataa <izzataa@code.edu.az>
2023-04-24 03:19:34 +0400

    Proje dosyası ekle.

---
+++ CustomList.Data/Customlist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList.Data
+{
+   
+    public class CustomList<T>
+    {
+        private List<T> items = new List<T>();
+
+       
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+       
+        public T Find(Predicate<T> function)
+        {
+            return items.Find(function);
+        }
+    }
+}
---
+++ CustomListApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CustomList.Core.Models; 
+using CustomList.Data;
+
+
+CustomList<Person> customList = new CustomList<Person>();
+customList.Add(new Person { Name = "Izzat", Surname = "Abdullayev", Age = 22, Position = "Senior" });
+customList.Add(new Person { Name = "Ahmed", Surname = "Mehdi", Age = 23, Position = "Senior" });
+customList.Add(new Person { Name = "Murad", Surname = "Safarli", Age = 26, Position = "Senior" });
+customList.Add(new Person { Name = "Anar", Surname = "Gurbanli", Age = 23, Position = "Senior" });
+customList.Add(new Person { Name = "Lala", Surname = "Firudinli", Age = 23, Position = "Senior" });
+
+Person person = customList.Find(person => person.Age == 25);
+if(person != null)
+{
+    Console.WriteLine($"{person.Name} {person.Surname}{person.Age}{person.Position}");
+}
+else
+{
+    Console.WriteLine("There is no such person");
+}
